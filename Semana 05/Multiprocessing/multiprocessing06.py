91086c4f1abfc8c083709d746f808b9aee75457b
Erick Oliveira <erickoliveirasystem@outlook.com>
2022-11-02 19:29:43 -0300

    Semana05: Threads and Multiprocessing

---
+++ Semana 05/Multiprocessing/multiprocessing06.py
@@ -0,0 +1,22 @@
+import time
+import concurrent.futures
+
+start = time.perf_counter()
+
+
+def do_something(seconds):
+    print(f"Sleeping {seconds} second(s) ...")
+    time.sleep(seconds)
+    return f"Done Sleeping...after {seconds} second(s)"
+
+
+with concurrent.futures.ProcessPoolExecutor() as executor:
+    secs = [5, 3, 2, 1]
+    results = executor.map(do_something, secs)  # return all in order of execution
+
+    for result in results:
+        print(result)
+
+finish = time.perf_counter()
+
+print(f"Finished in {round(finish-start, 2)} seconds(s)")
